65a7fe1f0adf0f1c837e22c014ecc166f8c31305
sweety <gnanamanisweety@gmail.com>
2024-12-13 01:21:21 +0530

    initialcommit

--- myapp/src/SimpleComponent.js
+++ myapp/src/SimpleComponent.js
@@ -4,7 +4,7 @@ class SimpleComponent extends Component {
   render() {
     return (
       <div>
-        <h1 style={{ color: "blue", fontSize: "24px", backgroundColor:"darkgrey",textAlign:"center" }}><b>Class Components</b></h1>
+        <h1 style={{ color: "blue", fontSize: "24px", backgroundColor:"darkgrey",textAlign:"center",border:"2px black solid" }}><b>Class Components</b></h1>
         <p style={{ color: "black", fontSize: "16px", backgroundColor:"lightcyan" }}>
         A class component in React is a blueprint for creating stateful components. It is an ES6 class that extends React. Component and includes a render() method to return JSX. Class components can maintain state and use lifecycle methods to handle component behavior.
         </p>
